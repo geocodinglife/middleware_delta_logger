cb5fed3586e17ab5a713f6387f5d9e9fe1bd33b3
geocodinglife <geocodinglife@gmail.com>
2021-09-23 11:50:40 -0500

    Authentication done

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -1,11 +1,13 @@
 class Admin::ApplicationController < ApplicationController
   before_action :authorize
-  
+
   def current_user
-    false
+    @user ||= User.find(session[:current_user_id]) if session[:current_user_id]
   end
 
   def authorize
-    redirect_to '/login', alert: 'Please login to view admin pages'
+    unless current_user
+      redirect_to '/login', alert: 'Please login to view admin pages'
+    end
   end
 end
